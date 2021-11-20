ff2f5af7ec607afb7d911550028a18b95873ef32
aantonenkov <78919513+aantonekov@users.noreply.github.com>
2021-11-20 14:55:28 +0200

    edit2

--- work1/src/App/index.js
+++ work1/src/App/index.js
@@ -16,23 +16,17 @@ function App() {
       <Header/>
 
       <Routes>
+        
         <Route exact path = '/aboutUs' element= {<AboutUsPage/>}/>
-      </Routes>
-
-      <Routes>
-        <Route exact path = '/aboutUs/InfoHistry' element= {<InfoHistry/>}/>
-      </Routes>
       
-      <Routes>
+        <Route exact path = '/aboutUs/InfoHistry' element= {<InfoHistry/>}/>     
+  
         <Route exact path = '/aboutUs/OurAchievements' element= {<OurAchievements/>}/>
-      </Routes>
 
-      <Routes>
         <Route exact path = '/preatyCat' element= {<PrettyCatPage/>}/>
-      </Routes>
 
-      <Routes>
         <Route exact path = '/myPhoto' element= {<MyPhotoPage/>}/>
+
       </Routes>
 
       <Footer/>
